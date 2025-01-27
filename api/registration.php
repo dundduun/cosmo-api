74bb4daab876951f9af19c82cf196bf5586105cc
dundduun <sadfgqr@gmail.com>
2025-01-27 13:19:51 +0300

    feat: add email exastance validation

--- api/registration.php
+++ api/registration.php
@@ -1,18 +1,28 @@
 <?php
+require_once('../utils/echoError.php');
 // При обращении к этому файлу по адресу webprof-kupriun.unpe.ru/api/registration.php сервер возвращает 200, исправить
 
 function registration(PDO $db, $input, $pathParts, $method) {
     if ($pathParts[1]) {
-        echoRes(['message' => 'Invalid request']); // Не соответствует требованиям, временная мера
+        echoRes(['message' => 'Invalid request, $pathParts[1] exists']); // Не соответствует требованиям
         return;
     } else if ($method !== 'POST') {
-        echoRes(['message' => 'Invalid request']); // Не соответствует требованиям, временная мера
+        echoRes(['message' => 'Invalid request, $method !== POST']); // Не соответствует требованиям
         return;
     }
 
-    // if () {} // проверить существование данных в $input
+    $checkEmailExistance = 'SELECT * FROM users WHERE email = :email';
+    
+    $checkEmailStmt = $db->prepare($checkEmailExistance);
+    $checkEmailStmt->execute(["email" => $input->email]);
 
-    $sql = 'INSERT INTO users (
+    if ((bool) $checkEmailStmt->fetch()) {
+        http_response_code(409);
+        echoValidationError(409, 'Conflict', []); // не передаёт ошибку
+        return;
+    }
+
+    $regSql = 'INSERT INTO users (
             first_name, 
             last_name, 
             patronymic, 
@@ -32,8 +42,8 @@ function registration(PDO $db, $input, $pathParts, $method) {
         )
     ';
 
-    $stmt = $db->prepare($sql);
-    $stmt->execute([
+    $regStmt = $db->prepare($regSql);
+    $regStmt->execute([
         'first_name' => $input->first_name,
         'last_name' => $input->last_name,
         'patronymic' => $input->patronymic,
@@ -43,5 +53,17 @@ function registration(PDO $db, $input, $pathParts, $method) {
         'token' => '',
     ]);
 
-    echoRes(['message' => 'success!']); // Не соответствует требованиям, временная мера
+    http_response_code(201);
+
+    echoRes(
+        ['data' => [
+                "user" => [
+                    "name" => "$input->last_name $input->first_name $input->patronymic",
+                    "email" => $input->email
+                ],
+                "code" => 201,
+                "message" => "Пользователь создан"
+            ]
+        ]
+    );
 }
